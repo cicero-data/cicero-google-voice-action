f598a96c84d26cc21875dfd642bd07f9011e7c3e
Kathryn Killebrew <kkillebrew@azavea.com>
2017-07-27 12:32:42 -0400

    Install actions client library
    Which is not the apiai npm client.

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 /*jshint esversion: 6 */
 
 exports.handler = (event, context, callback) => {
-    response = 'Hello webhook world!';
+    var response = 'Hello webhook world!';
+    var requestData = event.body;
+    console.log(requestData);
     callback(null, {statusCode: 200,
                     isBase64Encoded: false,
                     headers: {'Content-Type': 'application/json'},
