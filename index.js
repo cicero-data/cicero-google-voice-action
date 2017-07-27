c360408eaaa7d5d28c66fa073e838b22f2c285a2
Kathryn Killebrew <kkillebrew@azavea.com>
2017-07-27 15:13:39 -0400

    Add eslint configuration

--- index.js
+++ index.js
@@ -2,11 +2,12 @@
 
 exports.handler = (event, context, callback) => {
     var response = 'Hello webhook world!';
-    var requestData = event.body;
+    console.log(event.body);
+    var requestData = JSON.parse(event.body);
     console.log(requestData);
     callback(null, {statusCode: 200,
-                    isBase64Encoded: false,
-                    headers: {'Content-Type': 'application/json'},
-                    body: JSON.stringify({'speech': response, 'displayText': response })
+        isBase64Encoded: false,
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({'speech': response, 'displayText': response })
     });
 };
